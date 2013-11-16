3101cfed2ab56b2e458d67cf42bfce6145534cb8
Christopher Johnstone <cpj97@juno.com>
2013-11-16 15:16:49 -0500

    Added working code with the recorder module and updated the config file to represent the new options. Now, the recorder should do auto-record well.

--- Orbitersdk/Simpit/Simpit/PanelClickRecorderOutput.cpp
+++ Orbitersdk/Simpit/Simpit/PanelClickRecorderOutput.cpp
@@ -3,23 +3,53 @@
 void PanelClickRecorderOutput::load(FILEHANDLE inputFile)
 {
 	//inputFile = this->readToSectionStart(inputFile, "PANEL_CLICK_RECORDER");
+
+	//set up initial conditions
 	record = false;
+	currentUserDefId = 0;
+	strcpy(logName,"Modules\\Plugin\\SimpitRecorderLog.log");
+	strcpy(recordLogName,"EventRecorderOutput.log");
+
 	char *line;
 	while (oapiReadScenario_nextline(inputFile,line))
 	{
 		if (line[0] != ';')
 		{
 			//now see if it is our vessel name
-			if (sscanf(line,"VESSEL_NAME = %s", vesselName) == 1)
-			{
-				//set up hooking
-				
-			}
+			sscanf(line,"VESSEL_NAME = %s", vesselName);
+
+			//record the starting record id if it is there
+			sscanf(line,"STARTING_RECORD_ID = %i",&currentUserDefId);
+
 			//see if recording should be enabled
 			if (sscanf(line,"AUTO_RECORD = 1") == 1)
 			{
 				record = true;
 			}
+
+			//read log file info, if they want to change the defaults
+			sscanf(line,"EVENT_LOG_NAME = \"%254[^\"]\"",logName);
+
+			//print a starter so we can see where a session begins
+			FILE * logFile = fopen(logName,"a");
+			if (logFile != NULL)
+			{
+				fprintf(logFile,"Event logging started\n");
+				fclose(logFile);
+			}
+
+			//read record output info
+			sscanf(line,"RECORDING_LOG_NAME = \"%254[^\"]\"",recordLogName);
+
+
+			FILE * recordFile = fopen(recordLogName,"a");
+			if (recordFile != NULL)
+			{
+				fprintf(recordFile,"Recording started\n");
+				fclose(recordFile);
+			}
+
+
 		}
 	}
 }
@@ -28,11 +58,71 @@ void PanelClickRecorderOutput::SimulationStart()
 {
 	OBJHANDLE hVessel = oapiGetVesselByName(vesselName);
 	if (oapiIsVessel(hVessel))
-		InstallVesselHook(&(VESSEL2)oapiGetVesselInterface(hVessel));
+	{
+		InstallVesselHook((VESSEL2 *)oapiGetVesselInterface(hVessel));
+		observer.setUpReciever(this);
+		
+	}
 }
+
+void PanelClickRecorderOutput::handlePanelMouseEvent(int id, int ev, int mx, int my)
+{
+	if (record)
+	{
+		recordingQueue.push(new PanelMouseEvent(id,ev,mx,my));
+	}
+		
+	//log it
+	FILE * logFile = fopen(logName,"a");
+	if (logFile != NULL)
+	{
+		fprintf(logFile,"%d,%d,%d,%d\n",id,ev,mx,my);
+		fclose(logFile);
+	}
+}
+
 
 void PanelClickRecorderOutput::handleEvent(Event ev)
 {
+	//pop from queue, write to file
+	if (record && !recordingQueue.size())
+	{
+		FILE * recordFile = fopen(recordLogName,"a");
+		if (recordFile != NULL)
+		{
+			//we have to output two things, first a user definition.
+				//User def is in the format 
+				//defId	(groups of definitions)
+				//Each definition is
+				//	eventState	mx	my	mouseEvent
+			//then we have to output a event mapping
+				//mapping is in the format
+				//eventId defId clickId
+
+			//we get eventId and eventState from the event from handleEvent
+			//we get the four values, clickId, mx, my, and mouseEvent from the queue
+			//We need a recorder variable to record a new defId
+
+			//get our top mouseEvent
+			PanelMouseEvent * mouseEvent = recordingQueue.front();
+
+			//first set up the user definition
+			fprintf(recordFile,"%i %i %i %i %i\n",currentUserDefId, ev.state, mouseEvent->mx,
+				mouseEvent->my, mouseEvent->mouseEvent);
+
+			//now print out the event mapping
+			fprintf(recordFile,"%i %i %i\n", ev.id, currentUserDefId, mouseEvent->id);
+			fclose(recordFile);
+
+			//increment currentUserDefId so we don't overwrite it and clean up
+			currentUserDefId++;
+			
+			delete mouseEvent;
+			recordingQueue.pop();
+		}
+	}
+
+
 }
 
 void PanelClickRecorderOutput::FocusChanged(OBJHANDLE old_focus, OBJHANDLE new_focus)
--- Orbitersdk/Simpit/Simpit/PanelClickRecorderOutput.h
+++ Orbitersdk/Simpit/Simpit/PanelClickRecorderOutput.h
@@ -1,28 +1,40 @@
 #ifndef PANEL_CLICK_RECORDER_OUTPUT
 #define PANEL_CLICK_RECORDER_OUTPUT
 
+#include <queue>
+#include <string.h>
+
+#include "HookObserver.h"
+#include "PanelMouseEvent.h"
 #include "Output.h"
 #include "OrbiterAPI.h"
 #include "Orbitersdk.h"
 #include "VesselHook.h"
+#include "HookMethods.h"
 
 
 class PanelClickRecorderOutput : public Output
 {
 public:
-	PanelClickRecorderOutput(boost::function<void (Event)> _handle) : Output(_handle) {handleEventFunc = _handle;}
+	PanelClickRecorderOutput(boost::function<void (Event)> _handle) :  Output(_handle) {handleEventFunc = _handle;}
 	void load(FILEHANDLE inputFile);
 	void save(FILEHANDLE outputFile) {}
 	void SimulationStart();
 	void SimulationEnd();
 	void FocusChanged(OBJHANDLE old_focus, OBJHANDLE new_focus);
+	void handlePanelMouseEvent(int id, int ev, int mx, int my);
 	void handleEvent(Event ev);
 
 
 
+
 private:
+	std::queue<PanelMouseEvent*> recordingQueue;
+	char logName [256];
+	char recordLogName [256];
 	char vesselName[255];
 	bool record;
+	int currentUserDefId;
 };
 
 #endif
