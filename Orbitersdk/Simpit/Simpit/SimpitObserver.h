33e1aa8672b09c5de0bc327b0b3d75d650efe843
Christopher Johnstone <cpj97@juno.com>
2013-11-18 18:04:16 -0500

    Added the SimpitObserver class that will be used to remove the boost dependecy. Use it as a static class, all methods and variables are static to avoid global variables :)

---
+++ Orbitersdk/Simpit/Simpit/SimpitObserver.h
@@ -0,0 +1,17 @@
+#ifndef SIMPIT_OBSERVER
+#define SIMPIT_OBSERVER
+
+#include "Event.h"
+#include "SimpitManager.h"
+
+class SimpitObserver
+{
+public:
+	static void setUpManager(SimpitManager * _manager);
+	static void handleEvent(Event ev);
+private:
+	static SimpitManager * manager;
+};
+
+
+#endif
