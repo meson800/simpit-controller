77398e967b7f6c38015a38a9c60fc5224b8506b4
Christopher Johnstone <cpj97@juno.com>
2013-12-18 19:57:20 -0500

    Added KeyOutput class module, beginning of #8

---
+++ Orbitersdk/Simpit/Simpit/KeyOutput.cpp
@@ -0,0 +1,104 @@
+#include "KeyOutput.h"
+
+void KeyOutput::load(const char * key, const char * value)
+{
+	if (strcmp(key, "output") == 0)
+	{
+		int id, state;
+		Event ev;
+		char keys[256] = "";
+		char vesselClass[256]= "";
+		if (sscanf(value,"%i %i %256s %256s", &id, &state, keys, vesselClass) >= 3)
+		{
+			ev.id = id;
+			ev.state = state;
+			std::string s_keys = keys;
+			std::string s_vesselClass = vesselClass;
+
+			outputs[ev] = std::make_pair(s_vesselClass, s_keys);
+		}
+	}
+}
+
+void KeyOutput::handleEvent(Event ev)
+{
+	if (outputs.count(ev) != 0)
+	{
+		//get focus vessel
+		VESSEL * vessel = oapiGetFocusInterface();
+
+		if (outputs[ev].first.compare("") != 0)
+		{
+			//if there was a provided class, only trigger the keypresses if the focus vessel
+			//class equals the stored one
+			if (outputs[ev].first.compare(vessel->GetClassNameA()) == 0)
+			{
+				//trigger the event
+				SendStringAuto(outputs[ev].second.c_str());
+			}
+		}
+		else
+		{
+			//always trigger the keypresses
+			SendStringAuto(outputs[ev].second.c_str());
+		}
+
+	}
+}
+
+void KeyOutput::SendStringAuto(const char * string)
+{
+	//first send keydown
+	SendString(string, 0);
+	//then send keyup
+	SendString(string, KEYEVENTF_KEYUP);
+}
+
+void KeyOutput::SendString(const char * string, DWORD dwFlags)
+{
+
+	for (unsigned int i = 0; i < strlen(string); i++)
+	{
+		//some of the following adapted from
+		//http://batchloaf.wordpress.com/2012/04/17/simulating-a-keystroke-in-win32-c-or-c-using-sendinput/
+
+		INPUT ip;
+		//generic setup
+		ip.type = INPUT_KEYBOARD;
+		ip.ki.wScan = 0;
+		ip.ki.time = 0;	
+		ip.ki.dwExtraInfo = 0;
+		
+		WORD virtual_keycode;
+		//switch character and convert to keycode
+
+		//uses same format as AutoHotKey
+		//http://www.autohotkey.com/docs/commands/Send.htm
+		//for the modifier keys
+		switch (string[i])
+		{
+			case '+':
+				//shift
+				virtual_keycode = 0x10;
+				break;
+			case '^':
+				//control
+				virtual_keycode = 0x11;
+			case '!':
+				//alt
+				virtual_keycode = 0x12;
+				break;
+			case '#':
+				//windows key
+				virtual_keycode = 0x5B;
+			default:
+				//use VkKeyScan to convert keycode
+				virtual_keycode = VkKeyScan(string[i]);
+		}
+		
+		//send keystroke
+		ip.ki.wVk = virtual_keycode;
+		ip.ki.dwFlags = dwFlags;
+		SendInput(1, &ip, sizeof(INPUT));
+	}
+}
---
+++ Orbitersdk/Simpit/Simpit/KeyOutput.h
@@ -0,0 +1,28 @@
+#ifndef KEY_OUTPUT
+#define KEY_OUTPUT
+
+#include <map>
+#include <string>
+#include <Windows.h>
+#include <OrbiterAPI.h>
+
+#include "Output.h"
+#include "Event.h"
+
+class KeyOutput : public Output
+{
+public:
+	KeyOutput() : Output() {}
+	void load(const char * key, const char * value);
+	void handleEvent(Event ev);
+
+private:
+	void SendString(const char * string, DWORD dwFlags);
+	void SendStringAuto(const char * string);
+
+	//pair is between vesselClass and keys
+	std::map<Event,std::pair<std::string, std::string>> outputs;
+
+};
+
+#endif
