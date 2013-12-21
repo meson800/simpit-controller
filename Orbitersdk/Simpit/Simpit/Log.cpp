3b31115a4d92a10d7893e797d4ca498ed204e4d3
Christopher Johnstone <cpj97@juno.com>
2013-12-21 13:35:40 -0500

    Defined all Log functions Added files into the project file

--- Orbitersdk/Simpit/Simpit/Log.cpp
+++ Orbitersdk/Simpit/Simpit/Log.cpp
@@ -1,1 +1,43 @@
 #include "Log.h"
+
+FILE * Log::logHandle = 0;
+
+void Log::initLogging()
+{
+	if (logHandle == 0)
+		fopen("simpit_controller.log", "w");
+}
+
+void Log::shutdownLogging()
+{
+	if (logHandle != 0)
+		fclose(logHandle);
+	logHandle = 0;
+}
+
+void Log::print(const char * string)
+{
+	if (logHandle != 0)
+		fprintf(logHandle, "%s", string);
+}
+
+void Log::print(std::string string)
+{
+	print(string.c_str());
+}
+
+void Log::println(const char * string)
+{
+	if (logHandle != 0)
+		fprintf(logHandle, "%s\n", string);
+}
+
+void Log::println(std::string string)
+{
+	println(string.c_str());
+}
+
+FILE * Log::returnLogHandle()
+{
+	return logHandle;
+}
--- Orbitersdk/Simpit/Simpit/Log.h
+++ Orbitersdk/Simpit/Simpit/Log.h
@@ -1,4 +1,21 @@
 #ifndef LOG_CLASS
 #define LOG_CLASS
 
+#include <Windows.h>
+#include <string>
+
+class Log
+{
+public:
+	static void initLogging();
+	static void shutdownLogging();
+	static void print(const char * string);
+	static void print(std::string string);
+	static void println(const char * string);
+	static void println(std::string string);
+	static FILE * returnLogHandle();
+private:
+	static FILE * logHandle;
+};
+
 #endif
